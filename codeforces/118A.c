4ec4c831677f9a2bd756736f7c1600b736ae1b82
forhad <forhadsh1@gmail.com>
2020-11-30 01:07:41 +0600

    git commit -m

---
+++ codeforces/118A.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+   char string[100],str[100];
+   int i,k;
+   gets(string);
+   strlwr(string);
+   for(i=0; i<strlen(string); i++){
+      if(string[i]!= 'a' && string[i]!='e' && string[i]!='i' && string[i]!='o' && string[i]!='u'&& string[i]!='y'){
+        printf(".%c",string[i]);
+      }
+   }
+ 
+   return  0;
+}
