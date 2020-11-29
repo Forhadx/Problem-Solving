e935e1069768c0e53a1121b062fab65cac5f9df9
forhad <forhadsh1@gmail.com>
2020-11-30 01:24:09 +0600

    greedy, implementation, sortings

---
+++ codeforces/405A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cin>>n;
+    int i,arr[n];
+ 
+    for(i=0; i<n; i++)
+        cin>>arr[i];
+ 
+    sort(arr,arr+n);
+ 
+    for(i=0; i<n; i++)
+        cout<<arr[i]<<" ";
+    return 0;
+}
